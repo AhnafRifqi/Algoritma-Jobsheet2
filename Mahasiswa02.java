f03ecfad63f23737a10c6c57888a00e83ff4b0c6
AhnafRifqi <ahnaf.rifqi1412@gmail.com>
2025-02-18 14:49:24 +0700

    Mahasiswa02

---
+++ Mahasiswa02.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Mahasiswa02 {
+    class Mahasiswa {
+        private String nim;
+        private String nama;
+        private String kelas;
+        private double ipk;
+    
+        public Mahasiswa(String nim, String nama, String kelas, double ipk) {
+            this.nim = nim;
+            this.nama = nama;
+            this.kelas = kelas;
+            this.ipk = ipk;
+        }
+    
+        public void tampilkanInformasi() {
+            System.out.println("NIM: " + nim);
+            System.out.println("Nama: " + nama);
+            System.out.println("Kelas: " + kelas);
+            System.out.println("IPK: " + ipk);
+        }
+    
+        public void ubahKelas(String kelasBaru) {
+            this.kelas = kelasBaru;
+            System.out.println("Kelas berhasil diubah menjadi: " + kelas);
+        }
+    
+        public void updateIpk(double ipkBaru) {
+            this.ipk = ipkBaru;
+            System.out.println("IPK berhasil diperbarui menjadi: " + ipk);
+        }
+    
+        public String nilaiKinerja(double ipk) {
+            if (ipk >= 3.5) {
+                return "Sangat Baik";
+            } else if (ipk >= 3.0) {
+                return "Baik";
+            } else if (ipk >= 2.5) {
+                return "Cukup";
+            } else {
+                return "Kurang";
+            }
+        }
+    }
+}
